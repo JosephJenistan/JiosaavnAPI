2e9f9ddede8352b1121eb2540fd9dfa5411ca24e
JosephJenistan <73983918+JosephJenistan@users.noreply.github.com>
2023-02-26 12:34:27 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const package = require("../package.json");
 module.exports = async (req, res) => {
   res.json({
     alive: true,
-    made_by: "https://github.com/tuhinpal",
+    made_by: "https://github.com/JosephJenistan",
     version: package.version,
-    docs: `${process.env.APP_URL || "http://localhost:3000"}/docs`,
+    docs: "https://jenistan.gitbook.io/jiosaavn-api/",
   });
 };
